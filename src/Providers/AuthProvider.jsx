a31fc0f630398f95631b12453449dbd7a4cd7fd7
Siam-al-islam <siamalislam692@gmail.com>
2024-06-30 23:34:04 +0600

    sign up and login firebase functinality added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { getAuth } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import app from "../Firebase/firebase.config";
 
 
@@ -8,11 +8,34 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('currentUser: ', currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unSubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
-        loading
+        loading,
+        createUser,
+        signIn
     }
 
     return (
